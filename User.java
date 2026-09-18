c212d92890d105b3ba0456ff4c93046f0a4a52da
Harold Aquino <harold.aquino@unmsm.edu.pe>
2026-09-18 10:17:51 -0500

    Nuevo commit

--- User.java
+++ User.java
@@ -1,36 +1,36 @@
-public class User {
+    public class User {
 
-    private Long id;
-    private String name;
-    private String email;
-    private String password;
+        private Long id;
+        private String name;
+        private String email;
+        private String password;
 
-    public User(Long id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
+        public User(Long id, String name, String email, String password) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+
+        public void printInfo() {
+            System.out.println("Usuario: " + name);
+            System.out.println("Email: " + email);
+        }
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-
-    public void printInfo() {
-        System.out.println("Usuario: " + name);
-        System.out.println("Email: " + email);
-    }
-}
